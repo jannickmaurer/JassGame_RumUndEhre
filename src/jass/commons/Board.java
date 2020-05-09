962731b6b3534460b4e885e6017d15ac9ee8dda6
Muster <blackjack@eclipso.ch>
2020-05-09 10:06:41 +0200

    Beginn Board Programmierung Start der Spielrunde

--- src/jass/commons/Board.java
+++ src/jass/commons/Board.java
@@ -1,14 +1,88 @@
 package jass.commons;
 
-public class Board {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
-	
-	
-	public Trumpf evaluateTrumpf() {
-		
-		
-		
-		return null;
+import jass.client.TableCards;
+import jass.client.HandCards;
+import jass.commons.Card.Suit;
+
+public class Board {
+	public static int playersTurn = 1;
+	public int imPlayer;
+	public static String trumpf;
+
+	HandCards handCards = new HandCards();
+	TableCards tableCards = new TableCards();
+
+	public void enterPlayRoom(String roomName) { // Kommt von Sämi wenn spielraum einsteigt
+		// TODO server abfragen, wenn beigetretten wird
+		// ev als string kette 6d;6h;9d;th split(";") trennen googlen
+		handCards.add(new Card("D6"));
+		handCards.add(new Card("D7"));
+		handCards.add(new Card("DA"));
+		handCards.add(new Card("SQ"));
+		handCards.add(new Card("CT"));
+		handCards.add(new Card("S6"));
+		handCards.add(new Card("HJ"));
+		handCards.add(new Card("HK"));
+		handCards.add(new Card("S7"));
+
+		// aks Server witch player iam mit dem roomName
+		imPlayer = 1;
+
 	}
-	
+
+	void poll() {
+		Timer timer = new Timer();
+
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				// gettabelcards und getplayersturn beim server abrfragen
+				// über message server anfrage und rückmeldung der aktuellen karten
+				if (playersTurn == imPlayer) {
+					play();
+				}
+				if (tableCards.isComplete()) {
+					winnerEval();
+				}
+			}
+
+		}, 100, 1000);
+
+	}
+
+	private void play() {
+		if (trumpf == null) {
+			// logik um Trumpf zu bestimmen Rückmeldung view
+			// view box triggern welcher Spielmethode ob Trumpf obeAbe undeUfe, etc
+			trumpf = "H";
+		}
+
+		if (tableCards.hasCards()) {
+			// evaluieren was ich legen darf
+			// TODO evaluieren welche Karte gespielt werden darf anhand TableCards und
+			// HandCards
+		} else {
+			// TODO KArte darf gelegt werden direkt
+		}
+
+	}
+
+	private void winnerEval() {
+		// TODO Auto-generated method stub
+		//Jeder Client prüft selbst ob er gewonnen hat
+		// Danach gibt jeder Client den Punktestand zurück und SErver evaluiert welche Spieler zusammen
+		//gehören und passt die punktestände beider teams an
+		
+		/**
+		 * Wenn ich punkte meldete muss danach der Server das Board auf null setzen
+		 * clearen wenn alle vier karten liegen und server sagt welcher spieler anfängt, ist oben
+		 * implementiert
+		 */
+
+	}
 }
