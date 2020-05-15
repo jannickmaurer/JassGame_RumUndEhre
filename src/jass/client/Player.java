d10ede281a18439018a3ab213ff8bd0c08ace736
Muster <blackjack@eclipso.ch>
2020-05-15 11:00:19 +0200

    on hold, ausskommentiert

--- src/jass/client/Player.java
+++ src/jass/client/Player.java
@@ -1,15 +1,42 @@
 package jass.client;
 
+import java.util.ArrayList;
+
+import jass.commons.Card;
 import jass.commons.Trumpf;
 
 public class Player {
-
-//	private Trumpf trumpf;
-//	
+	
+//    private final int playerNumber; // This is the ID
+//    private final ArrayList<Card> cards = new ArrayList<>();
+//    private Trumpf trumpf; //Sage damit ob ich Trumpf oder Stich habe
+//    
+//    public Player(int playerNumber) {
+//        this.playerNumber = playerNumber;
+//    }
+//
+//    public int getPlayerNumber() {
+//        return playerNumber;
+//    }
+//
+//    public ArrayList<Card> getCards() {
+//        return cards;
+//    }
+//    
+//    public void discardHand() {
+//        cards.clear();
+//        trumpf = null;
+//    }
+//    
+//    public int getNumCards() {
+//        return cards.size();
+//    }
 //	
 //	public Trumpf evaluateTrumpf() {
+//		trumpf = Trumpf.evaluateTrumpf(cards);
 //		
-//		return null;
+//		
+//		return trumpf;
 //	}
-	
+//	
 }
