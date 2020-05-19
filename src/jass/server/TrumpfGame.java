80f1be610d6efc2b655ac43695f0a61a90872545
Muster <blackjack@eclipso.ch>
2020-05-19 19:20:15 +0200

    Add WinnerEvaluation Class

--- src/jass/server/TrumpfGame.java
+++ src/jass/server/TrumpfGame.java
@@ -2,6 +2,7 @@ package jass.server;
 
 import java.util.logging.Logger;
 
+import jass.commons.Card;
 import jass.commons.ServiceLocator;
 
 public class TrumpfGame extends Playroom {
@@ -12,6 +13,40 @@ public class TrumpfGame extends Playroom {
 		super(name, owner);
 	}
 	
+	public static String trumpf;
+	public static String gameTyp = "Trumpf";
+	private ServerTableCards serverTableCards;
+	
+	
+	public void addServerTableCard(String tableCard) {
+		serverTableCards.add(new Card (tableCard));
+		
+	}
+	
+	//Sieger bekommt alle Punkte und kann Anfangen
+	public void winnerIsPlayer() {
+		int playerWinnerNr;
+		if (gameTyp == "Trumpf") {
+			Card tempWinnerCard;
+			tempWinnerCard = serverTableCards.getHighestTrumpfCard();
+			for (int i = 0; i < serverTableCards.hasLength(); i++) {
+				if(serverTableCards.getCard(i).toString() == tempWinnerCard.toString()) {
+					playerWinnerNr = i;
+				}
+			}
+		}
+		if (gameTyp == "ObeAbe") {
+			Card tempWinnerCard;
+			
+		}
+		if (gameTyp == "UndeUfe") {}
+		if (gameTyp == "Slalom") {}
+	}
+	
+	//plus 5 punkte, wenn alle 36 Karten gespielt wurden
+	public int pointsForWinner() {
+		return serverTableCards.getPoints();//alle vier Karten mit Punkte zusammenzählen
+	}
 	
 	
 	
