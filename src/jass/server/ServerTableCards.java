78711807cc468eb3148c544c92bb61b20d02972e
Muster <blackjack@eclipso.ch>
2020-05-21 17:16:51 +0200

    addClientCard Methode fertig gestellt. fügt karte und username separaten listen hinzu

--- src/jass/server/ServerTableCards.java
+++ src/jass/server/ServerTableCards.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import jass.commons.Card;
 import jass.commons.Cards;
 import jass.commons.Trumpf;
-import jass.commons.Card.Suit;
 
 public class ServerTableCards extends Cards{
 
 	ArrayList<Card> serverTableCards = new ArrayList<>();
+	ArrayList<String> usernames = new ArrayList<>();
 	private Trumpf trumpf;
-	private Suit firstSuit;
+	
 
 	public ServerTableCards() {
 		super();
@@ -29,22 +29,15 @@ public class ServerTableCards extends Cards{
 		return serverTableCards.size() == 4;
 	}
 	
-
-	
-//	public Suit getFirstSuitOnServerTable() {
-//		firstSuit  = Trumpf.getFirstSuit(serverTableCards);
-//		return firstSuit;
-//	}
-	
 	public int getPoints() {
 		return Trumpf.getPoints(serverTableCards);
 	}
 	
-	
 	//Add a card to severTable in format: Jannick:D9
 	public void addClientCard(String clientCard) {
 		String[] temp = clientCard.split("\\:");
-		
+		usernames.add(temp[0]);
+		serverTableCards.add(new Card(temp[1]));
 	}
 	
 	public void clearServerTableCards() {
