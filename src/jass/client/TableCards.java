24dc968834b2651e533d745e2e3d7a8347ac4517
Muster <blackjack@eclipso.ch>
2020-05-24 21:42:44 +0200

    samll changes

--- src/jass/client/TableCards.java
+++ src/jass/client/TableCards.java
@@ -1,40 +1,28 @@
 package jass.client;
 
 import java.util.ArrayList;
-
 import jass.commons.Card;
 import jass.commons.Card.Suit;
 import jass.commons.Trumpf;
 
 public class TableCards{
 	ArrayList<Card> tableCards = new ArrayList<>();
-//	private Trumpf trumpf;
-	private Suit firstSuit;
 
-	public TableCards() {
-		super();
-	}
+//	public TableCards() {
+//		super();
+//	}
 
 	public ArrayList<Card> getTableCards() {
 		return tableCards;
 	}
-	
-//	public boolean hasCards() {
-//		return tableCards.size() != 0; //boolean evaluation ohne if direkt 0 = True
-//	}
 
-	public boolean isComplete() { // brauchts die hier???
+	public boolean isComplete() { 
 		return tableCards.size() == 4;
 	}
 	
 	public Trumpf evaluateTrumpf() {
 		return Trumpf.evaluateTrumpf(tableCards);
 	}
-	
-//	public Suit getFirstSuit() {
-//		firstSuit  = Trumpf.getFirstSuit(tableCards);
-//		return firstSuit;
-//	}
 	
 	public boolean hasCards() {
 		return tableCards.size() != 0; 
@@ -58,5 +46,4 @@ public class TableCards{
 	public Card getCard(int i) {
 	return tableCards.get(i);
 	}
-	
 }
