21033c66dd495d41c4b9ea715ff81d205cfedc12
Muster <blackjack@eclipso.ch>
2020-05-24 21:29:01 +0200

    board anpassungen

--- src/jass/commons/Board.java
+++ src/jass/commons/Board.java
@@ -1,18 +1,10 @@
 package jass.commons;
 
-import static org.junit.jupiter.api.Assumptions.assumingThat;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.logging.Logger;
-
 import jass.client.TableCards;
 import jass.client.HandCards;
 import jass.client.PlayableHandCards;
-import jass.client.Player;
-import jass.commons.Card.Suit;
 
 public class Board {
 	private static ServiceLocator sl = ServiceLocator.getServiceLocator();
@@ -32,49 +24,25 @@ public class Board {
 	private int playedCards;
 	private ArrayList<String> members = new ArrayList<>();
 	
-//	HandCards handCards;
-
 	public Board(String roomName, String gameTyp) {
 		this.handCards = new HandCards();
 		this.tableCards = new TableCards();
 		this.gameTyp = gameTyp;
 		this.playedCards = 0;
 		this.playableHandCards = new PlayableHandCards();
-//		this.enterPlayRoom(roomName);
 	}
 
-//	public void enterPlayRoom(String roomName) {
-//		// TODO server abfragen, wenn beigetretten wird
-//		// identisch wie tabelCArdsFrom SErver adden für imPlayer und
-//		imPlayer = 1;	
-//		// ask Server witch player i am
-//		imPlayer = 2;
-//	}
-//	
 	public void shuffledCardListener(String newHandCards) {
 		ArrayList<Card> tempHandCards = new ArrayList<>();
 		logger.info(newHandCards);
 		String[] handCardList = newHandCards.split("\\|");
 		for (int i = 0; i < handCardList.length; i++) {
 			tempHandCards.add(new Card(handCardList[i]));
-
-	//		this.handCards.add(new Card(handCardList[i]));
-//			this.startHandCards.add(new Card (handCardList[i]));
-			//this.startHandCards.add(new Card (handCardList[i]));
 		}
 		handCards.setHandCards(tempHandCards);
 		logger.info("HandCards added: " + handCards.toString());
 		
 	}
-	
-//	public void playerListener(int playersTurn) {
-//		Board.playersTurn = playersTurn;
-//
-//		if (Board.playersTurn == imPlayer) { // erfragt welcher player am zug ist, falls identisch kann player
-//												// spielen
-//			play();
-//		}
-//	}
 
 	//Methode play evaluiert nur welche Karten gespielt werden dürfen und welche nicht
 	public void play() {
@@ -149,47 +117,6 @@ public class Board {
 		logger.info("Play: " + playableHandCards + "  " + gameTyp + "  " + trumpf);
 	}
 
-//	private void selectGameVariety() {
-		// Methode evaluieren was der Spieler als GAmeVariante gewählt hat
-		// zB Trumpf, ObeAbe, UndeUfe, Slalom
-		// TODO Auto-generated method stub
-	//	this.gameVariety = gameVariety;
-//	}
-
-//	public Trumpf evaluateTrumpf() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-
-//	public int winnerEval() {
-////		if (!wiisDone)
-////			wiisEval();
-//
-//		// TODO Auto-generated method sub
-//		// Jeder Client prüft selbst ob er gewonnen hat
-//		// Danach gibt jeder Client den Punktestand zurück und SErver evaluiert welche
-//		// Spieler zusammen
-//		// gehören und passt die punktestände beider teams an
-//
-//		/**
-//		 * Wenn ich punkte meldete muss danach der Server das Board auf null setzen
-//		 * clearen wenn alle vier karten liegen und server sagt welcher spieler anfängt,
-//		 * ist oben implementiert
-//		 */
-//		int winner = 1;
-//		//send winner to server;
-//		return winner;
-//	}
-
-//	private void wiisEval() {
-//		// TODO Auto-generated method stub
-//		if (!handCards.hasWiis()) wiisDone = true;
-//		else {
-//			//Server die Weisenums übergeben und den Weis an Sämi übergeben,
-//			//falls die Person weisen kann
-//		} wiisDone = true;
-//	}
-
 	public void addTableCard(Card card) {
 		logger.info("Add Card to TableCards: " + card.toString());
 		this.tableCards.addTableCard(card);
@@ -200,16 +127,6 @@ public class Board {
 			logger.info("TableCards cleared");
 		}
 	}
-	
-//	public String getPlayableCards() {//Mehoden mit playableCards werden 
-//		//momentan nicht verwendet, die anderen drei unten auch nicht
-//		return playableCards;
-//	}
-//	
-//
-//	public void setPlayableCards(String playableCards) {
-//		this.playableCards = playableCards;
-//	}
 
 	public int getImPlayer() {
 		return imPlayer;
@@ -247,20 +164,6 @@ public class Board {
 		return playableHandCards.getPlayableHandCards();
 	}
 
-	
-//	public void setPlayableHandCards(HandCards playableHandCards) {
-//		this.playableHandCards = playableHandCards;
-//	}
-
-//	public HandCards getRemainingHandCards() {
-//		return handCards;
-//	}
-
-//	public void setRemainingHandCards(HandCards remainingHandCards) {
-//		this.remainingHandCards = remainingHandCards;
-//	}
-
-	
 //Getter und Setter
 	public HandCards getHandCards() {
 		return handCards;
@@ -270,17 +173,9 @@ public class Board {
 		return handCards.getHandCards();
 	}
 
-//	public void setHandCards(HandCards handCards) {
-//		this.handCards = handCards;
-//	}
-
 	public static void setPlayersTurn(int playersTurn) {
 		Board.playersTurn = playersTurn;
 	}
-
-//	public void setTableCards(TableCards tableCards) {
-//		this.tableCards = tableCards;
-//	}
 	
 	public void removeHandCard(Card card) {
 		logger.info("Board removes handcard: " + card.toString() );
@@ -292,5 +187,4 @@ public class Board {
 			this.members.add(m);
 		}
 	}
-
 }
